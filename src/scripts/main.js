fc6d85c7ee318008ce3523dd8f9c7503d06e698e
Naimy441 <105175634+Naimy441@users.noreply.github.com>
2023-02-09 20:24:44 -0600

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,12 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 path = require('path')
+fs = require('fs')
+console = require('console')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        fullscreen: true,
+        frame: false,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
         },
@@ -20,7 +22,21 @@ function loadHTML(event, name) {
     win.loadFile('src/html/' + name + '.html')
 }
 
+function loadUserData(event) {
+    userDataFile = path.join(__dirname, '../', '../', 'user_data.json')
+    userDataFile = "C:/Users/abu/tsa-education-app/user_data.json"
+    rawData = fs.readFileSync(userDataFile)
+    userData = JSON.parse(rawData)
+    event.returnValue = userData
+}
+
+function print(event, message) {
+    console.log(message)
+}
+
 app.whenReady().then(() => {
     ipcMain.on('load-html', loadHTML)
+    ipcMain.on('load-user-data', loadUserData)
+    ipcMain.on('print', print)
     createWindow()
 })
