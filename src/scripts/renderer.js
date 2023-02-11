9233d06d574097e988b2fb160273944182295154
Naimy441 <105175634+Naimy441@users.noreply.github.com>
2023-02-11 00:04:38 -0600

    Update renderer.js

--- src/scripts/renderer.js
+++ src/scripts/renderer.js
@@ -1,24 +1,4 @@
-const about_us_button = document.getElementById('about_us')
-about_us_button.addEventListener('click', () => {
-    window.electronAPI.loadHTML('about_us')
-})
-
-const study_button = document.getElementById('study')
-study_button.addEventListener('click', () => {
-    window.electronAPI.loadHTML('study')
-})
-
 const notes_button = document.getElementById('notes')
 notes_button.addEventListener('click', () => {
     window.electronAPI.loadHTML('notes')
-})
-
-const timers_button = document.getElementById('timers')
-timers_button.addEventListener('click', () => {
-    window.electronAPI.loadHTML('timers')
-})
-
-const tracker_button = document.getElementById('tracker')
-tracker_button.addEventListener('click', () => {
-    window.electronAPI.loadHTML('tracker')
 })
