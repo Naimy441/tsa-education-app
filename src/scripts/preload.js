3c90f1d67308898fc539a565d87634d0d84f6799
Naimy441 <105175634+Naimy441@users.noreply.github.com>
2023-02-11 00:04:52 -0600

    Update preload.js

--- src/scripts/preload.js
+++ src/scripts/preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
     print: (message) => ipcRenderer.send('print', message),
     loadHTML: (filename) => ipcRenderer.send('load-html', filename),
-    loadUserData: () => ipcRenderer.sendSync('load-user-data')
+    loadUserData: () => ipcRenderer.sendSync('load-user-data'),
+    saveUserData: (jsonData) => ipcRenderer.send('save-user-data', jsonData),
 });
