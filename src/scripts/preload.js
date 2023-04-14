8254f942c9513f9842d599bea9290eeb8c98f9f7
Naimy441 <105175634+Naimy441@users.noreply.github.com>
2023-04-13 22:58:41 -0500

    finished?

--- src/scripts/preload.js
+++ src/scripts/preload.js
@@ -5,4 +5,7 @@ contextBridge.exposeInMainWorld('electronAPI', {
     loadHTML: (filename) => ipcRenderer.send('load-html', filename),
     loadUserData: () => ipcRenderer.sendSync('load-user-data'),
     saveUserData: (jsonData) => ipcRenderer.send('save-user-data', jsonData),
+    getCurrentDate: () => ipcRenderer.sendSync('get-current-date'),
+    getPythonData: (filename, request, jsonData) => ipcRenderer.send('get-python-data', filename, request, jsonData),
+    random: (min, max) => ipcRenderer.sendSync('random', min, max), 
 });
